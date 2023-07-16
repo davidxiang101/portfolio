086e8198198d2076e26c9e7fb7bae38956bb092a
davidxiang101 <davidxiang101@gmail.com>
2023-07-16 10:14:29 -0500

    build error fix

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -8,7 +8,7 @@ const About: React.FC = () => {
                 Fast-forward to the present day, and I&apos;ve been fortunate enough to bring dreams to life in VR. I&apos;ve worked on immersive VR applications that, quite honestly, feel like I&apos;m living out my Minecraft modding dreams on a grand scale.
             </p>
             <p className="text-md text-slate-400 leading-relaxed mt-4">
-                I&apos;ve also been exploring the complexities of AI and have found it to be as fascinating as it is challenging. I'm equally drawn to web development's creativity, crafting everything from freelance projects to random web games.
+                I&apos;ve also been exploring the complexities of AI and have found it to be as fascinating as it is challenging. I'm equally drawn to web development&apos;s creativity, crafting everything from freelance projects to random web games.
             </p>
             <p className="text-md text-slate-400 leading-relaxed mt-4">
                 When the screen&apos;s glow gets a bit too much, you&apos;ll find me in the kitchen, experimenting with new recipes and flavors. If I&apos;m not burning pasta, I&apos;m either out on the soccer field, feeling the adrenaline rush at the go-kart track, or on my couch, catching the latest F1 race.
--- app/components/projectsList.tsx
+++ app/components/projectsList.tsx
@@ -7,9 +7,8 @@ const ProjectsList: React.FC = () => {
     return (
         <div id="projects" className="flex flex-col items-center">
             {projects.map((project: ProjectProps, index: number) => (
-                <div className="w-1/2 mx-auto">
+                <div key={index} className="w-1/2 mx-auto">
                     <Project
-                        key={index}
                         name={project.name}
                         description={project.description}
                         link={project.link}
