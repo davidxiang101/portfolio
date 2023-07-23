b122e2ac73c35677ae3c3fa14f23702f03787f0a
davidxiang101 <davidxiang101@gmail.com>
2023-07-23 12:58:30 -0500

    card full link

--- app/components/project/project.tsx
+++ app/components/project/project.tsx
@@ -1,14 +1,16 @@
-import React, { PropsWithChildren } from 'react';
+import React from 'react';
 import { FaExternalLinkAlt } from 'react-icons/fa';
 import { ProjectProps } from '../utils/types';
 
 const Project: React.FC<ProjectProps> = ({ name, description, link }) => (
-    <div className="card flex border relative border-violet-500 shadow-lg px-4 py-[30%] justify-center">
-        <h2 className="z-10 font-bold text-xl w-1/2 text-center">{name}</h2>
-        <a href={link} className="absolute top-3 right-3 text-slate-400">
-            <FaExternalLinkAlt />
-        </a>
-    </div>
+    <a href={link} className="block">
+        <div className="card flex border relative border-violet-500 shadow-lg px-4 py-[30%] justify-center">
+            <h2 className="z-10 font-bold text-xl w-1/2 text-center">{name}</h2>
+            <div className="absolute top-3 right-3 text-slate-400">
+                <FaExternalLinkAlt />
+            </div>
+        </div>
+    </a>
 );
 
 export default Project;
--- app/data/projects.ts
+++ app/data/projects.ts
@@ -2,21 +2,21 @@ import { ProjectProps } from '../components/utils/types';
 
 export const projects: ProjectProps[] = [
     {
+        name: 'TeeTaste',
+        description: 'Developed a t-shirt recommendation engine based on head to head matchups of t-shirts',
+        link: 'https://github.com/davidxiang101/TeeTaste',
+        tags: ['Selenium', 'Pytorch', 'PostGres', 'NextJS', 'Tailwind']
+    },
+    {
         name: 'Bluetooth Location Tracking App',
         description: 'Designed and built a real-time location tracking app using Bluetooth Low Energy (BLE) technology. The app is capable of tracking and displaying the location of BLE devices in real time.',
-        link: 'https://github.com/YourGithubUsername/BluetoothLocationTracking',
-        tags: ['React Native', 'BLE', 'IoT']
+        link: '',
+        tags: ['Swift', 'SwiftUI', 'IoT']
     },
     {
         name: 'LiDAR Data Visualization',
         description: 'Created a comprehensive data visualization tool for interpreting LiDAR data. The tool features 3D visualization, easy navigation, and detailed analysis capabilities.',
-        link: 'https://github.com/YourGithubUsername/LiDARDataVisualization',
+        link: '',
         tags: ['Python', 'Pandas', 'Matplotlib', 'LiDAR']
-    },
-    {
-        name: 'TeeTaste',
-        description: 'Developed a full-featured e-commerce website with a custom built shopping cart, user authentication, and product management. Integrated with Stripe for payment processing.',
-        link: 'https://www.yoursite.com/',
-        tags: ['React', 'Node.js', 'MongoDB', 'Express', 'Stripe']
     },
 ];
