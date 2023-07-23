b8010bc5ccb1efa7f5c8d86ab103207a73950a9f
davidxiang101 <davidxiang101@gmail.com>
2023-07-23 12:27:02 -0500

    link icon added in project cards

--- app/components/project/project.tsx
+++ app/components/project/project.tsx
@@ -1,13 +1,14 @@
 import React, { PropsWithChildren } from 'react';
+import { FaLink } from 'react-icons/fa';
 import { ProjectProps } from '../utils/types';
 
 const Project: React.FC<ProjectProps> = ({ name, description, link }) => (
     <div className="card flex border relative border-violet-500 shadow-lg px-4 py-[30%] justify-center">
         <h2 className="z-10 font-bold text-xl w-1/2 text-center">{name}</h2>
-        {/* <p>{description}</p> */}
-        {/* <a href={link} className="text-blue-500 hover:underline">View Project</a> */}
+        <a href={link} className="absolute top-3 right-3 text-slate-400">
+            <FaLink />
+        </a>
     </div>
 );
-
 
 export default Project;
