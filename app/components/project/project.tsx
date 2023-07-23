e43e89dbcbbb69cf7c9b50a731208e13376979e4
davidxiang101 <davidxiang101@gmail.com>
2023-07-23 12:43:47 -0500

    link icon anim

--- app/components/project/project.tsx
+++ app/components/project/project.tsx
@@ -1,12 +1,12 @@
 import React, { PropsWithChildren } from 'react';
-import { FaLink } from 'react-icons/fa';
+import { FaExternalLinkAlt } from 'react-icons/fa';
 import { ProjectProps } from '../utils/types';
 
 const Project: React.FC<ProjectProps> = ({ name, description, link }) => (
     <div className="card flex border relative border-violet-500 shadow-lg px-4 py-[30%] justify-center">
         <h2 className="z-10 font-bold text-xl w-1/2 text-center">{name}</h2>
         <a href={link} className="absolute top-3 right-3 text-slate-400">
-            <FaLink />
+            <FaExternalLinkAlt />
         </a>
     </div>
 );
